52014dd2ae30a1e44474c833f233f5a2a15bfd08
JensenZ <3185640243@qq.com>
2026-06-28 21:49:07 +0800

    Ready to prod01

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'http://localhost:5173', // Your local dev environment
     'https://monmediacwiki.cc', // custom domain name
     'https://www.monmediacwiki.cc', // The www version of custom domain name
-    'https://moegirl-wiki-frontend.vercel.app' // original Vercel placeholder link
+    'https://moegirl-wiki-frontend-zhangmeizi-prs-projects.vercel.app' // original Vercel placeholder link
 ];
 
 module.exports = allowedOrigins;
