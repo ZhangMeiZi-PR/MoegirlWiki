c669ad3939e911cb3bf70e6448ac6e4a2a78616c
JensenZ <3185640243@qq.com>
2026-06-28 21:45:51 +0800

    Ready to prod

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,8 @@
 const allowedOrigins = [
-  'http://localhost:5173',
+    'http://localhost:5173', // Your local dev environment
+    'https://monmediacwiki.cc', // custom domain name
+    'https://www.monmediacwiki.cc', // The www version of custom domain name
+    'https://moegirl-wiki-frontend.vercel.app' // original Vercel placeholder link
 ];
 
 module.exports = allowedOrigins;
